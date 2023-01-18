5a3627dfdd5b586335d255bc51c1dfb4c5841d3b
Willian Murayama <willianyos13@gmail.com>
2023-01-18 17:00:19 -0300

    Implementação do trabalho
    Adição de comentários
    Correção da escrita da média no arquivo texto
    Implementação da obtenção dos dados no arquivo texto

--- Trabalho/main.c
+++ Trabalho/main.c
@@ -4,6 +4,7 @@
 
 #define MAXIMOALUNO 2
 
+// Parte 1 Construcao do struct
 typedef struct Nascimento {
     int dia;
     int mes;
@@ -87,6 +88,7 @@ void CalcularMedia(Aluno *Aluno) {
 }
 
 int main() {
+    // Parte 2 Leitura e alocacao dos dados no struct
     Aluno vetorAluno[5];
     float vetorTrabalhos[5], vetorProvas[5], vetorMedia[5];
     char arquivoOrigem[] = "teste.txt", arquivoDestino[] = "teste2.txt";
@@ -112,17 +114,19 @@ int main() {
         }
     }
     fclose(arquivo);
+    // Parte 3.1 calculo da media e resultado
     for (int i = 0; i < MAXIMOALUNO; i++) {
         CalcularMedia(&vetorAluno[i]);
         CalcularAprovacao(&vetorAluno[i]);
     }
 
+    // Parte 3.2 escrita do resultado e media
     arquivo = fopen(arquivoDestino, "w");
     for (int i = 0; i < MAXIMOALUNO; i++) {
         fprintf(arquivo, "%d\n", vetorAluno[i].registroAcademico);
         for (int j = 0; j < 5; j++) {
             fprintf(arquivo, "%s\n", vetorAluno[i].materias[j].nomeDisciplina);
-            fprintf(arquivo, "%f\n", vetorAluno[i].materias[j].media);
+            fprintf(arquivo, "%.2f\n", vetorAluno[i].materias[j].media);
             fprintf(arquivo, "%s\n", vetorAluno[i].materias[j].aprovado);
         }
     }
@@ -141,6 +145,33 @@ int main() {
             printf("Media de %s: %f\n", vetorAluno[alunoIndice].materias[i].nomeDisciplina, vetorAluno[alunoIndice].materias[i].media);
             printf("Aprovacao: %s\n", vetorAluno[alunoIndice].materias[i].aprovado);
             printf("\n");
+        }
+    }
+    // Parte 4 leitura do arquivo com media e resultado
+    arquivo = fopen(arquivoDestino, "r");
+    char nome[5 * MAXIMOALUNO][51], aprovado[5 * MAXIMOALUNO][20], disciplina[5 * MAXIMOALUNO][20];
+    int registroAcademico[MAXIMOALUNO];
+    float medias[5 * MAXIMOALUNO];
+
+    // fscanf(arquivo, "%d\n", registroAcademico[0]);
+    // printf("Registro Academico: %d\n", registroAcademico[0]);
+    for (int i = 0; i < MAXIMOALUNO; i++) {
+        fscanf(arquivo, "%d\n", &registroAcademico[i]);
+        for (int i = 0; i < 5; i++) {
+            fscanf(arquivo, "%s\n", disciplina[i]);
+            fscanf(arquivo, "%f\n", &medias[i]);
+            fscanf(arquivo, "%s\n", aprovado[i]);
+        }
+    }
+    fclose(arquivo);
+
+    printf("Parte 4\n");
+    for (int i = 0; i < MAXIMOALUNO; i++) {
+        printf("Registro Academico: %d\n", registroAcademico[i]);
+        for (int i = 0; i < 5; i++) {
+            printf("Disciplina: %s\n", disciplina[i]);
+            printf("Media da disciplina: %f\n", medias[i]);
+            printf("Situacao: %s\n\n", aprovado[i]);
         }
     }
 }
